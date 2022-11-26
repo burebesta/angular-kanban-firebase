df2a428ca755c85b55dce04f3c0b5862e0cb617d
Drake <decebrolus@gmail.com>
2022-11-26 11:49:54 +0200

    small fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyD22fnoVb5nf-ChBp5f8JA6GHRsiRWm0FA",
+    authDomain: "energymarket-8f805.firebaseapp.com",
+    projectId: "energymarket-8f805",
+    storageBucket: "energymarket-8f805.appspot.com",
+    messagingSenderId: "439126091585",
+    appId: "1:439126091585:web:8a0747a44da7da8ca1a3c0",
+    measurementId: "G-3PFNE6YF8T"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyD22fnoVb5nf-ChBp5f8JA6GHRsiRWm0FA",
+    authDomain: "energymarket-8f805.firebaseapp.com",
+    projectId: "energymarket-8f805",
+    storageBucket: "energymarket-8f805.appspot.com",
+    messagingSenderId: "439126091585",
+    appId: "1:439126091585:web:8a0747a44da7da8ca1a3c0",
+    measurementId: "G-3PFNE6YF8T"
+  }
 };
 
 /*
